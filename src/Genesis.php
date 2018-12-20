fd5c503051e67304270078fc0244e43c2311bf43
Zachary Watkins <watkinza@gmail.com>
2018-12-20 14:47:29 -0600

    Add unit header and custom header image support

--- src/Genesis.php
+++ src/Genesis.php
@@ -11,6 +11,8 @@ class Genesis {
 
 	public function __construct() {
 
+		add_theme_support( 'custom-header', array( 'width' => 1400, 'height' => 225 ) );
+
 		// Remove header widget area
 		unregister_sidebar( 'header-right' );
 
@@ -20,6 +22,10 @@ class Genesis {
 
 		// Replace site title with logo
 		add_filter( 'genesis_seo_title', array( $this, 'add_logo' ), 10, 3 );
+
+		// Add Unit Header Content
+		add_filter( 'genesis_structural_wrap-header', array( $this, 'unit_header' ), 999 );
+
 
 	}
 
@@ -35,6 +41,29 @@ class Genesis {
 		$title = str_replace( $inside, $new_inside, $title );
 
 		return $title;
+
+	}
+
+	public function unit_header( $output ){
+
+		ob_start();
+		genesis_seo_site_title();
+		$site_title = ob_get_clean();
+		$new_inside = sprintf( '<a href="%s">%s</a>', trailingslashit( home_url() ), get_bloginfo( 'name' ) );
+		$site_title = preg_replace( '/<a [^<]+<img [^<]+<\/a>/', $new_inside, $site_title );
+
+		ob_start();
+		genesis_seo_site_description();
+		$site_description = ob_get_clean();
+
+		$unit_header = sprintf( '<div class="unit-header-wrap" style="background-image:url(%s);"><div class="unit-header layout-container"><div class="wrap">%s</div></div></div>',
+			get_header_image(),
+			"{$site_title}{$site_description}"
+		);
+
+		$output = preg_replace('/<\/div>$/', $unit_header . '</div>', $output);
+
+		return $output;
 
 	}
 
