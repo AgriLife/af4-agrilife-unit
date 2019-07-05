2bcf5f20c7ada1290b18e1d57995a415e7f5cc01
Zachary Watkins <watkinza@gmail.com>
2019-07-05 08:35:35 -0500

    Remove Google font since it is loaded by agriflex4

--- templates/communications-home.php
+++ templates/communications-home.php
@@ -16,28 +16,8 @@
 remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
 remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_open', 5 );
 remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_close', 15 );
-add_action( 'wp_enqueue_scripts', 'af4_unit_fonts' );
 add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_full_width_content' );
 add_action( 'genesis_after_entry', 'agrilife_unit_com_home' );
-
-/**
- * Registers all styles used within the plugin
- *
- * @since 0.1.0
- * @return void
- */
-function af4_unit_fonts() {
-
-	wp_register_style(
-		'agrilife-unit-googlefonts',
-		'https://fonts.googleapis.com/css?family=Oswald',
-		array(),
-		'1.0.0'
-	);
-
-	wp_enqueue_style( 'agrilife-unit-googlefonts' );
-
-}
 
 /**
  * Output content of template.
