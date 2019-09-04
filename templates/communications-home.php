531f813e5844af93e57c0cdeb93dfbeecd185c76
Zachary Watkins <watkinza@gmail.com>
2019-09-04 07:58:24 -0500

    Fix home page layout issue

--- templates/communications-home.php
+++ templates/communications-home.php
@@ -17,7 +17,23 @@ remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
 remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_open', 5 );
 remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_close', 15 );
 add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_full_width_content' );
+add_filter( 'genesis_structural_wrap-site-inner', 'agrilife_unit_site_inner_wrap' );
 add_action( 'genesis_after_entry', 'agrilife_unit_com_home' );
+
+/**
+ * Add grid-container class name
+ *
+ * @since 1.1.2
+ * @param string $output The wrap HTML.
+ * @return string
+ */
+function agrilife_unit_site_inner_wrap( $output ) {
+
+	$output = str_replace( 'grid-container', 'grid-container full', $output );
+
+	return $output;
+
+}
 
 /**
  * Output content of template.
