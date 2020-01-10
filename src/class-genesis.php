a569e20374a8678e6b4cdbfeb731cc04ea2ec36a
Zachary Watkins <watkinza@gmail.com>
2020-01-10 11:57:07 -0600

    Change site header content for mobile, remove the magnifying glass toggle, and remove the word “Menu” from the mobile version of the site header

--- src/class-genesis.php
+++ src/class-genesis.php
@@ -28,6 +28,8 @@ class Genesis {
 	 */
 	public function __construct() {
 
+		global $af_required;
+
 		add_theme_support(
 			'custom-header',
 			array(
@@ -38,6 +40,18 @@ class Genesis {
 
 		// Add Unit Header Content.
 		add_filter( 'genesis_structural_wrap-header', array( $this, 'unit_header' ), 999 );
+
+		// Add logo and site title for mobile.
+		add_filter( 'af4_header_logo', array( $this, 'genesis_seo_title' ), 11, 3 );
+		add_filter( 'genesis_attr_title-area', array( $this, 'class_cell_title_area' ), 11 );
+		add_filter( 'af4_before_nav', array( $this, 'change_menu_toggle' ), 10 );
+		add_filter( 'af4_before_nav', array( $this, 'change_title_bar_cell' ), 10 );
+		remove_filter( 'af4_before_nav', array( $af_required, 'add_search_toggle' ), 11 );
+
+		// Move right header widget area attached to the AgriFlex\RequiredDOM class.
+		remove_filter( 'af4_before_nav', array( $af_required, 'add_search_toggle' ), 11 );
+		remove_action( 'genesis_header', array( $af_required, 'add_header_right_widgets' ), 10 );
+		add_filter( 'af4_primary_nav_menu', array( $this, 'add_search_widget' ), 9 );
 
 	}
 
@@ -69,6 +83,106 @@ class Genesis {
 		$output = preg_replace( '/<\/div>$/', '</div>' . $unit_header, $output );
 
 		return $output;
+
+	}
+
+	/**
+	 * Change site title content.
+	 *
+	 * @since 1.2.0
+	 * @param string $title Genesis SEO title html.
+	 * @param string $inside The inner HTML of the title.
+	 * @param string $wrap The tag name of the seo title wrap element.
+	 * @return string
+	 */
+	public function genesis_seo_title( $title, $inside, $wrap ) {
+
+		$mobile_title = sprintf(
+			'<span class="cell shrink agrilife-mobile-logo show-for-small-only"><img src="%simages/AgriLife-A.png"></span><span class="cell auto site-title-text hide-for-medium">%s</span>',
+			ALUAF4_DIR_URL,
+			get_bloginfo( 'name ')
+		);
+		$title = str_replace( '</a>', $mobile_title . '</a>', $title );
+		$title = str_replace( '<a ', '<a class="grid-x" ', $title );
+
+		// Hide main logo on mobile.
+		preg_match_all( '/<img[^>]+>/', $title, $matches, PREG_SET_ORDER );
+		foreach ( $matches as $image ) {
+			if ( false !== strpos( $image[0], '/logo-agrilife.png' ) ) {
+				if ( strpos( $image[0], ' class="' ) ) {
+					$new_main_logo = str_replace( ' class="', ' class="hide-for-small-only', $image[0] );
+				} else {
+					$new_main_logo = str_replace( '<img ', '<img class="hide-for-small-only" ', $image[0] );
+				}
+
+				$title = str_replace( $image[0], $new_main_logo, $title );
+			}
+		}
+
+		return $title;
+
+	}
+
+	/**
+	 * Add header title area cell class names
+	 *
+	 * @since 1.2.4
+	 * @param array $attributes HTML attributes.
+	 * @return array
+	 */
+	public function class_cell_title_area( $attributes ) {
+
+		$attributes['class'] = str_replace( 'small-6 medium-2', 'auto', $attributes['class'] );
+		return $attributes;
+
+	}
+
+	/**
+	 * Change header menu toggle
+	 *
+	 * @since 1.2.4
+	 * @param string $output Output for af4_before_nav_args.
+	 * @return string
+	 */
+	public function change_menu_toggle( $output = '' ) {
+
+		$output = str_replace( '<div class="title-bar-title" data-toggle="nav-menu-primary">Menu</div>', '', $output );
+		return $output;
+
+	}
+
+	/**
+	 * Change title bar cell class.
+	 *
+	 * @since 1.2.4
+	 * @param string $output Output for af4_before_nav_args.
+	 * @return string
+	 */
+	public function change_title_bar_cell( $output = '' ) {
+
+		$output = str_replace( '<div class="title-bars cell small-6', '<div class="title-bars cell shrink', $output );
+		return $output;
+
+	}
+
+	/**
+	 * Add search widget and toggle button.
+	 *
+	 * @since 1.2.4
+	 * @param string $output Output for the primary menu.
+	 * @return string
+	 */
+	public function add_search_widget( $output ) {
+
+		global $af_required;
+
+		$search  = '<div class="title-bars cell medium-shrink title-bar-right">';
+		$search .= '<div class="title-bar title-bar-search"><button class="search-icon" type="button" data-toggle="header-search"></button><div class="title-bar-title">Search</div>';
+		$search  = $af_required->add_header_right_widgets( $search );
+		$search  = str_replace( 'id="header-search', 'data-toggler=".hide-for-medium" id="header-search', $search );
+		$search .= '</div></div>';
+
+		return $output . $search;
 
 	}
 
